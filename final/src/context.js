34165ace2c339e3152433730575e6ad7a88bda73
smajaas <smajaas@gmail.com>
2023-06-27 11:35:52 +0530

    fetch questions

--- final/src/context.js
+++ final/src/context.js
@@ -1,24 +1,74 @@
-import axios from 'axios'
-import React, { useState, useContext, useEffect } from 'react'
+import axios from 'axios';
+import React, { useState, useContext, useEffect } from 'react';
 
 const table = {
   sports: 21,
   history: 23,
   politics: 24,
-}
+};
 
-const API_ENDPOINT = 'https://opentdb.com/api.php?'
+const API_ENDPOINT = 'https://opentdb.com/api.php?';
 
-const url = ''
+const url = '';
 
-const AppContext = React.createContext()
+const tempUrl =
+  'https://opentdb.com/api.php?amount=10&category=18&difficulty=medium&type=multiple';
+
+const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
-}
+  const [waiting, setWaiting] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [index, setIndex] = useState(0);
+  const [correct, setCorrect] = useState(0);
+  const [error, setError] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const fetchQuestions = async (url) => {
+    setLoading(true);
+    setWaiting(false);
+
+    const response = await axios(url).catch((err) => console.log(err));
+    if (response) {
+      const data = response.data.results;
+      if (data.length > 0) {
+        setQuestions(data);
+        setLoading(false);
+        setWaiting(false);
+        setError(false);
+      } else {
+        setWaiting(true);
+        setError(true);
+      }
+    } else {
+      setWaiting(true);
+    }
+  };
+
+  useEffect(() => {
+    fetchQuestions(tempUrl);
+  }, []);
+
+  return (
+    <AppContext.Provider
+      value={{
+        waiting,
+        loading,
+        questions,
+        index,
+        correct,
+        error,
+        isModalOpen,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
 // make sure use
 export const useGlobalContext = () => {
-  return useContext(AppContext)
-}
+  return useContext(AppContext);
+};
 
-export { AppContext, AppProvider }
+export { AppContext, AppProvider };
